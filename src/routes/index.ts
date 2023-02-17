7ad813760d034127374da7a0f8721aee3680edf2
donaldcrane <donaldagbakoba@gmail.com>
2023-02-17 21:03:54 +0100

    add working directories

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router as expRouter } from "express";
 import studentRoutes from "./student";
+import userRoutes from "./user";
 
 const router = expRouter();
 
 router.use("/students", studentRoutes);
+router.use("/users", userRoutes);
 
 export default router;
